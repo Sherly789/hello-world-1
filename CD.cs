bc8370f3a7bca952ef6b091ab5d3f9c02d9593b9
Russ Davies and Shokouh Farvid <russ+shokouh@>
2016-07-12 15:04:17 -0700

    add private class to car folder

---
+++ CD.cs
@@ -0,0 +1,49 @@
+using System;
+
+class Cd
+{
+  private string _title;
+  private string _artist;
+  private int _price;
+
+  public void SetTitle(string newTitle)
+  {
+    _title = newTitle;
+  }
+  public string GetTitle()
+  {
+    return _title;
+  }
+
+  public void SetArtist(string newArtist)
+  {
+    _artist = newArtist;
+  }
+  public string GetArtist()
+  {
+    return _artist;
+  }
+
+  public void SetPrice(int newPrice)
+  {
+    _price = newPrice;
+  }
+  public int GetPrice()
+  {
+    return _price;
+  }
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    Cd anotherCd = new Cd();
+    anotherCd.SetTitle("Rubber Soul");
+    anotherCd.SetArtist("The Beatles");
+    anotherCd.SetPrice(-10);
+    Console.WriteLine(anotherCd.GetTitle());
+    Console.WriteLine("By: " + anotherCd.GetArtist());
+    Console.WriteLine("Price: $" + anotherCd.GetPrice());
+  }
+}
--- car.cs
+++ car.cs
@@ -3,39 +3,61 @@ using System.Collections.Generic;
 
 class Car
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
+  private string _MakeModel;
+  private int _Price;
+  private int _Miles;
+
+  public void SetMakeModel(string newMakeModel)
+  {
+    _MakeModel = newMakeModel;
+  }
+  public string GetMakeModel()
+  {
+    return _MakeModel;
+  }
+
+  public void SetPrice(int newPrice)
+  {
+    _Price = newPrice;
+  }
+  public int GetPrice()
+  {
+    return _Price;
+  }
+
+  public void SetMiles(int newMiles)
+  {
+    _Miles = newMiles;
+  }
+  public int GetMiles()
+  {
+    return _Miles;
+  }
+
+  public Car(string carModel, int carPrice, int carMiles)
+  {
+    _MakeModel = carModel;
+    _Price = carPrice;
+    _Miles = carMiles;
+  }
 }
 
 public class Program
 {
   public static void Main()
   {
-    Car porsche = new Car();
-    porsche.MakeModel = "2014 Porsche 911";
-    porsche.Price = 114991;
-    porsche.Miles = 7864;
+    Car porsche = new Car("2014 Porsche 911", 114991, 7864);
 
-    Car ford = new Car();
-    ford.MakeModel = "2011 Ford F450";
-    ford.Price = 55995;
-    ford.Miles = 14241;
+    Car ford = new Car("2011 Ford F450", 55995, 14241);
 
-    Car lexus = new Car();
-    lexus.MakeModel = "2013 Lexus RX 350";
-    lexus.Price = 44700;
-    lexus.Miles = 20000;
+    Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
 
-    Car mercedes = new Car();
-    mercedes.MakeModel = "Mercedes Benz CLS550";
-    mercedes.Price = 39900;
-    mercedes.Miles = 37979;
+    Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
     List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
     foreach(Car automobile in Cars)
     {
-      Console.WriteLine(automobile.MakeModel);
+      Console.WriteLine(automobile.GetMakeModel());
     }
 
     Console.WriteLine("Enter maximum price: ");
@@ -46,7 +68,7 @@ public class Program
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.Price < maxPrice)
+      if (automobile.GetPrice() < maxPrice)
       {
         CarsMatchingSearch.Add(automobile);
       }
@@ -54,7 +76,7 @@ public class Program
 
     foreach(Car automobile in CarsMatchingSearch)
     {
-      Console.WriteLine(automobile.MakeModel);
+      Console.WriteLine(automobile.GetMakeModel());
     }
   }
 }
